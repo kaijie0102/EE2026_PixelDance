bd08174943b22d291b277d14e7c1ef19833c437d
kai jie <leowkaijie@gmail.com>
2024-04-06 19:22:16 +0800

    Make play area narrower

--- project_improvement.srcs/sources_1/new/random_x_generator1.v
+++ project_improvement.srcs/sources_1/new/random_x_generator1.v
@@ -49,6 +49,8 @@ module random_x_generator1(
             end else begin
                 random_x <= lfsr_reg[7:1]; // Directly use the value if it's 80 or below
             end
+            
+            if (random_x < 10) random_x = random_x + 10;
         end 
     end
 endmodule
